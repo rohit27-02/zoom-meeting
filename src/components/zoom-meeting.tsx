41d06cb6da657c13151b38725e46c836b1415bf2
rohit rawat <rohitrawatop@gmail.com>
2024-01-06 23:57:45 +0530

    style

--- src/components/zoom-meeting.tsx
+++ src/components/zoom-meeting.tsx
@@ -83,7 +83,7 @@ const ZoomMeeting: React.FC<{ meetingDetails: MeetingDetails }> = ({
     }
 
     return (
-        <div className="flex z-[9999] gap-8">
+        <div className="flex z-[9999] fixed top-0 bg-orange-500 text-xl p-4 text-white gap-8">
             <a href={`https://zoom.us/oauth/authorize?response_type=code&client_id=pOJF_dfeStiKIvxWYF36ig&redirect_uri=${process.env.NEXT_PUBLIC_URL}`}>authorize</a>
             <button onClick={startMeeting}>Start Meeting</button>
             <button onClick={joinMeeting}>Join Meeting</button>
