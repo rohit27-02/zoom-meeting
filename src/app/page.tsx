2cef9e153760184a868e403714ef20a1ac7da643
rohit rawat <rohitrawatop@gmail.com>
2024-01-10 00:18:09 +0530

    ss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
-import ScheduleMeetingForm from '@/components/schedule-meeting-form';
 const DynamicZoomMeeting = dynamic(() => import('@/components/zoom-meeting'), {
   ssr: false, // Prevent server-side rendering
 });
