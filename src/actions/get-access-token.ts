b557865d3c5244145bfc0b67218957468825bb12
rohit rawat <rohitrawatop@gmail.com>
2024-01-09 12:51:23 +0530

    c

--- src/actions/get-access-token.ts
+++ src/actions/get-access-token.ts
@@ -3,7 +3,7 @@ export const getaccessToken = async ({authToken}:{authToken:string}) => {
     const data = new URLSearchParams();
     data.append("code", authToken);
     data.append("grant_type", "authorization_code");
-    data.append("redirect_uri", process.env.NEXT_PUBLIC_URL || "");
+    data.append("redirect_uri", `${process.env.NEXT_PUBLIC_URL}/schedule` || "");
     data.append("client_id", "0RG_LglYTBS2kvwVDiAYw");
     data.append("client_secret",'a1Gh3vHjChowcQBQo3uUwCigO2XZEsG4');
     const res = await fetch("https://zoom.us/oauth/token", {
